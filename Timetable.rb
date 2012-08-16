5e79624bea05f61dac773a89ce85e8b7d86985d0
ShaunOfTheLive <shaun.hill@gmail.com>
2012-08-16 19:41:51 -0400

    add more planning comments

--- Timetable.rb
+++ Timetable.rb
@@ -1,6 +1,10 @@
 class Timetable
   def initialize(blocks)
   #TODO: if list of lists, make each sub-list into a Block
+  # actually, for EACH element, we must check whether it's enumerable
+  # and if it is, use it to construct a Block
+  # or check if it's already a Block
+  # otherwise, throw an exception
     if @blocks.respond_to?('each')
       @blocks = blocks
     else
