4ea9a9cdcc2bb3ea5cacbc00fa7d71e1161ad8b4
ShaunOfTheLive <shaun.hill@gmail.com>
2012-08-16 04:09:46 -0400

    retabbed

--- Timetable.rb
+++ Timetable.rb
@@ -1,14 +1,14 @@
 class Timetable
-	def initialize(blocks)
-		if @blocks.respond_to?('each')
-		  @blocks = blocks
-		else
-			raise "Timetable must be constructed with a list!"
-		end
+  def initialize(blocks)
+    if @blocks.respond_to?('each')
+      @blocks = blocks
+    else
+      raise "Timetable must be constructed with a list!"
+    end
   end
 
-	def to_a
-		@blocks
-	end
+  def to_a
+    @blocks
+  end
 end
 
