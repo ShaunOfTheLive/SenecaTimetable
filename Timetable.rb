eb9ff907ab2ca183940513468e7a51aaac96aa04
ShaunOfTheLive <shaun.hill@gmail.com>
2012-08-16 19:59:25 -0400

    implement constructor

--- Timetable.rb
+++ Timetable.rb
@@ -5,10 +5,16 @@ class Timetable
   # and if it is, use it to construct a Block
   # or check if it's already a Block
   # otherwise, throw an exception
-    if @blocks.respond_to?('each')
-      @blocks = blocks
-    else
-      raise "Timetable must be constructed with a list!"
+    raise "Timetable must be constructed with a list!" if !blocks.respond_to?('each')
+    blocks.map do |block|
+      if block.is_a? Block
+        block
+      elsif block.respond_to?('each')
+        Block.new(block)
+      else
+        raise "Element must be a Block, or respond to 'each'"
+      end
+    end
     end
   end
 
