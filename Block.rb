2efc40af50e83b9637f24ca9238faf31e1ceee2e
ShaunOfTheLive <shaun.hill@gmail.com>
2012-08-17 00:55:40 -0400

    accept different numbers of arguments (temporary measure)

--- Block.rb
+++ Block.rb
@@ -1,15 +1,29 @@
 class Block
-  attr_accessor :course, :professor, :room, :colour
+  attr_accessor :course, :professor, :room, :colour, :string
 
-  def initialize(course, professor, room, colour = nil)
-    @course = course
-    @professor = professor
-    @room = room
-    @colour = colour
+  def initialize(*args)
+    case args.length
+    when 0
+      raise "You must pass at least one argument to the constructor!"
+    when 1
+      @string = args[0]
+    when 3..4
+      @course = course
+      @professor = professor
+      @room = room
+    when 4
+      @colour = colour
+    else
+      raise "Illegal number of arguments!"
+    end
   end
 
   def to_a
-    [@course, @professor, @room, @colour]
+    if defined?(@string)
+      return [@string]
+    else
+      return [@course, @professor, @room, @colour]
+    end
   end
 end
 
