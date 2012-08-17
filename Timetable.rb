7dc20eb586dcba1a765416d105628b52939ca311
ShaunOfTheLive <shaun.hill@gmail.com>
2012-08-17 00:44:25 -0400

    update comments

--- Timetable.rb
+++ Timetable.rb
@@ -2,17 +2,23 @@ require_relative 'Block'
 
 class Timetable
   def initialize(blocks)
-  #TODO: if list of lists, make each sub-list into a Block
-  # actually, for EACH element, we must check whether it's enumerable
+  # acceptable inputs:
+  # blocks: a 2D array of Blocks, or a 2D array of Arrays
+  # (Arrays will be converted into Blocks)
+
+  # TODO: for each 2-deep element, we must check whether it's enumerable
   # and if it is, use it to construct a Block
   # or check if it's already a Block
   # otherwise, throw an exception
+
+  # @blocks is a 2D array (rows, cols) of Blocks
     raise "Timetable must be constructed with a list!" if !blocks.respond_to?('each')
     blocks.map do |block|
       if block.is_a? Block
         block
       elsif block.respond_to?('each')
-        Block.new(block)
+        print block
+        Block.new(*block)
       else
         raise "Element must be a Block, or respond to 'each'"
       end
