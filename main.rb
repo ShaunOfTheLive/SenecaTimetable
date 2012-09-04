e54fb94ee2b56bdd93060729c718c86ed39e1607
ShaunOfTheLive <shaun.hill@gmail.com>
2012-09-04 14:37:23 -0400

    moved methods from extract_data.rb into a class called Parser, and renamed them

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require_relative 'extract_data'
+require_relative 'Parser'
 require_relative 'create_html_table'
 require_relative 'Timetable'
 require_relative 'LocalReader'
@@ -8,12 +8,12 @@ htmlDoc = LocalReader.read("docs/SIRIS Timetable.html")
 
 #-- extract header data and print to console (will use later)
 #-- Name, date, semester, etc.
-#extract_header_data(htmlDoc).each { |header| print header + "\n" }
+#Parser.parseHeaders(htmlDoc).each { |header| print header + "\n" }
 #print "\n"
 
 #-- extract days and print to console (will use later)
 #-- Monday, Tuesday...
-#extract_days(htmlDoc).each { |day| print day + "\n" }
+#Parser.parseDays(htmlDoc).each { |day| print day + "\n" }
 #print "\n"
 
 # read local template file into html_template string
@@ -21,8 +21,8 @@ html_template = LocalReader.read("docs/timetable_template.html")
 
 # extract timetable data, and feed it to create_html_table,
 # along with template
-#html_out = create_html_table(extract_timetable_data(htmlDoc), html_template)
-tt = Timetable.new(extract_timetable_data(htmlDoc))
+#html_out = create_html_table(Parser.parseBody(htmlDoc), html_template)
+tt = Timetable.new(Parser.parseBody(htmlDoc))
 html_out = create_html_table(tt.to_a, html_template)
 
 #-- debug: print final HTML to console
