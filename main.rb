ec57064294967cfe3ab8184baf60502f5f58dadf
ShaunOfTheLive <shaun.hill@gmail.com>
2012-11-09 13:39:43 -0500

    get rid of crap

--- main.rb
+++ main.rb
@@ -14,16 +14,6 @@ require_relative 'LocalWriter'
 # read local HTML into htmlDoc string
 htmlDoc = LocalReader.read("docs/SIRIS Timetable.html")
 
-#-- extract header data and print to console (will use later)
-#-- Name, date, semester, etc.
-#Parser.parseHeaders(htmlDoc).each { |header| print header + "\n" }
-#print "\n"
-
-#-- extract days and print to console (will use later)
-#-- Monday, Tuesday...
-#Parser.parseDays(htmlDoc).each { |day| print day + "\n" }
-#print "\n"
-
 tt = Timetable.new(Parser.parseBody(htmlDoc))
 html_template = LocalReader.read("docs/timetable_template.html")
 html_out = HtmlFormatter.format(tt.to_a, html_template)
