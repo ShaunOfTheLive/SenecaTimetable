f077b2c184b779a9152d2603f3e236ea09c0bc9e
ShaunOfTheLive <shaun.hill@gmail.com>
2012-11-09 14:12:05 -0500

    clarify slightly

--- Timetable.rb
+++ Timetable.rb
@@ -1,6 +1,9 @@
 require_relative 'Block'
 
 class Timetable
+  # @blocks is a 2D array (rows, cols) of Blocks
+  @blocks
+
   def initialize(blocks)
   # acceptable inputs:
   # blocks: a 2D array of Blocks, or a 2D array of Arrays
@@ -11,7 +14,6 @@ class Timetable
   # or check if it's already a Block
   # otherwise, throw an exception
 
-  # @blocks is a 2D array (rows, cols) of Blocks
     raise "Timetable must be constructed with a list!" if !blocks.respond_to?('each')
     @blocks = blocks.map do |row|
       row.map do |cell|
