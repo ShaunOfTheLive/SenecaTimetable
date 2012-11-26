d9e9e57b62f0d70f63d73200ea3b0093c57bd2ae
ShaunOfTheLive <shaun.hill@gmail.com>
2012-11-26 03:22:29 -0500

    delete TODO comment, fix typo

--- Timetable.rb
+++ Timetable.rb
@@ -9,11 +9,6 @@ class Timetable
   # events: a 2D array of Events, or a 2D array of Arrays
   # (Arrays will be converted into Events)
 
-  # TODO: for each 2-deep element, we must check whether it's enumerable
-  # and if it is, use it to construct a Event
-  # or check if it's already a Event
-  # otherwise, throw an exception
-
     raise "Timetable must be constructed with a list!" if !events.respond_to?('each')
     @eventHash = events.map do |row|
       row.map do |cell|
@@ -22,7 +17,7 @@ class Timetable
         elsif cell.respond_to?('each')
           Event.new(*cell)
         else
-          raise "2-deep element must be a Event, or respond to 'each'"
+          raise "2-deep element must be an Event, or respond to 'each'"
         end
       end
     end
